4fe00fbbed9caff6564c25e866764abcf5226b5e
micha.meder@gmail.com <micha.meder@gmail.com@a6d0b3e4-eef0-53aa-bf60-0259151e4dc4>
2011-09-14 15:49:58 +0000

    added sample text and now displaying some output from the Stanford Named Entity Recognizer (NER) after pressing the submit button

--- trunk/isslod-ed/src/eu/lod/ed/web/EdServlet.java
+++ trunk/isslod-ed/src/eu/lod/ed/web/EdServlet.java
@@ -1,13 +1,22 @@
 package eu.lod.ed.web;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
 
+import javax.management.RuntimeErrorException;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import eu.lod.ed.ner.StandfordEntity;
+import eu.lod.ed.text.TextProcessingFacade;
+import eu.lod.ed.text.impl.DefaultTextProcessingFacade;
 
 /**
  * Servlet implementation class EdServlet
@@ -15,6 +24,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(description = "a simple servlet", urlPatterns = { "/EdServlet" })
 public class EdServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private TextProcessingFacade textFacade;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -24,6 +34,23 @@ public class EdServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		InputStream is = config.getServletContext().getResourceAsStream("WEB-INF/ner-eng-ie.crf-4-conll.ser.gz");
+		try {
+			textFacade = new DefaultTextProcessingFacade(new GZIPInputStream(is));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally{
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -32,13 +59,18 @@ public class EdServlet extends HttpServlet {
 		/*
 		* Set the content type(MIME Type) of the response.
 		*/
+		String input = request.getParameter("input");
+		System.out.println(input);
+		List<StandfordEntity> entities = textFacade.getEntities(input);
+		
 		response.setContentType("text/html");
 		 
 		PrintWriter out = response.getWriter();
 		/*
 		* Write the HTML to the response
 		*/
-		out.println("some output");
+		
+		out.println(entities);
 		out.close();
 	}
 
