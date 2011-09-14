7a715b287919dd0da13777dc8872dad4a7b132c0
aldvinaldvin@gmail.com <aldvinaldvin@gmail.com@a6d0b3e4-eef0-53aa-bf60-0259151e4dc4>
2011-09-14 12:40:54 +0000

    + TextProcessingFacade interface and its default implementation

--- isslod-ed/src/eu/lod/ed/ner/StandfordEntity.java
+++ isslod-ed/src/eu/lod/ed/ner/StandfordEntity.java
@@ -1,34 +1,50 @@
 package eu.lod.ed.ner;
 
-public class StandfordEntity {
-    private String name;
-    private String type;
-    private int start;
-    private int end;
-    
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getType() {
-        return type;
-    }
-    public void setType(String type) {
-        this.type = type;
-    }
-    public int getStart() {
-        return start;
-    }
-    public void setStart(int start) {
-        this.start = start;
-    }
-    public int getEnd() {
-        return end;
-    }
-    public void setEnd(int end) {
-        this.end = end;
-    }
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
+public class StandfordEntity {
+	private String name;
+	private String type;
+	private int start;
+	private int end;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public void setStart(int start) {
+		this.start = start;
+	}
+
+	public int getEnd() {
+		return end;
+	}
+
+	public void setEnd(int end) {
+		this.end = end;
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+				.append("name", name).append("type", type)
+				.append("start", start).append("end", end).toString();
+	}
 }
