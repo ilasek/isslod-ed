da6698ec8aeb834f07d24d2b266bb059e84bc724
aldvinaldvin@gmail.com <aldvinaldvin@gmail.com@a6d0b3e4-eef0-53aa-bf60-0259151e4dc4>
2011-09-15 07:48:44 +0000

    + stopwords removal + new "listStems" method in TPF + PorterStemmer + etc

--- trunk/isslod-ed/src/eu/lod/ed/text/impl/DefaultTextProcessingFacade.java
+++ trunk/isslod-ed/src/eu/lod/ed/text/impl/DefaultTextProcessingFacade.java
@@ -4,6 +4,7 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.zip.GZIPInputStream;
@@ -14,12 +15,17 @@ import edu.stanford.nlp.ling.CoreAnnotations.AnswerAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.BeginPositionAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.EndPositionAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.Word;
+import edu.stanford.nlp.process.PTBTokenizer;
 import eu.lod.ed.ner.StandfordEntity;
 import eu.lod.ed.text.TextProcessingFacade;
+import eu.lod.stemmer.PorterStemmer;
+import eu.lod.stopwords.Stopwords;
 
 public class DefaultTextProcessingFacade implements TextProcessingFacade {
 
 	private AbstractSequenceClassifier classifier;
+	private PorterStemmer stemmer;
 
 	public DefaultTextProcessingFacade(InputStream classifierStream) {
 		try {
@@ -33,6 +39,8 @@ public class DefaultTextProcessingFacade implements TextProcessingFacade {
 			} catch (IOException e) {
 			}
 		}
+		// init stemmer
+		stemmer = new PorterStemmer();
 	}
 
 	@Override
@@ -60,10 +68,12 @@ public class DefaultTextProcessingFacade implements TextProcessingFacade {
 						prevEntity.setType(guessedAnswer);
 						prevEntity.setStart(label
 								.get(BeginPositionAnnotation.class));
-						prevEntity.setEnd(label.get(EndPositionAnnotation.class));
+						prevEntity.setEnd(label
+								.get(EndPositionAnnotation.class));
 						prevEntity.setName(label.word());
 					} else {
-						prevEntity.setEnd(label.get(EndPositionAnnotation.class));
+						prevEntity.setEnd(label
+								.get(EndPositionAnnotation.class));
 						prevEntity.setName(prevEntity.getName() + ' '
 								+ label.word());
 					}
@@ -79,35 +89,54 @@ public class DefaultTextProcessingFacade implements TextProcessingFacade {
 		return resultList;
 	}
 
-	public static void main(String[] args) {
-		try {
-			String input = "Carl Edward Sagan (November 9, 1934 – December 20, 1996) "
-					+ "was an American astronomer, astrophysicist, cosmologist, author, science popularizer, "
-					+ "and science communicator in the space and natural sciences.";
-			InputStream is = new GZIPInputStream(new BufferedInputStream(
-					new FileInputStream(
-							"classifiers/ner-eng-ie.crf-4-conll.ser.gz")));
-			DefaultTextProcessingFacade tf = new DefaultTextProcessingFacade(is);
-			
-			System.out.println(tf.getEntities(input));
-			/*
-			List<List<CoreLabel>> sentences = tf.classifier.classify(input);
-			for (List<CoreLabel> sentence : sentences) {
-				System.out.println("-----");
-				for (CoreLabel label : sentence) {
-					System.out.print(label.word());
-					System.out.print(":");
-					System.out.println(label
-							.get(CoreAnnotations.AnswerAnnotation.class));
-				}
-				System.out.println("-----");
+	@Override
+	public List<String> listStems(String input) {
+		StringReader inputReader = new StringReader(input);
+		PTBTokenizer<Word> tokenizer = PTBTokenizer
+				.newPTBTokenizer(inputReader);
+		List<String> result = new LinkedList<String>();
+		while (tokenizer.hasNext()) {
+			Word curWord = tokenizer.next();
+			String wordStr = curWord.word();
+			if (wordStr == null || wordStr.trim().length() < 2) {
+				continue;
 			}
+			if (Stopwords.isStopword(wordStr)) {
+				continue;
+			}
+			result.add(stemmer.stemWord(wordStr));
+		}
+		return result;
+	}
 
-			System.out.println(tf.classifier.classifyToCharacterOffsets(input));
-			*/
+	public static void main(String[] args) {
+		String input = "Carl Edward Sagan (November 9, 1934 – December 20, 1996) "
+				+ "was an American astronomer, astrophysicist, cosmologist, author, science popularizer, "
+				+ "and science communicator in the space and natural sciences.";
+		try {
+			InputStream is = new GZIPInputStream(
+					new BufferedInputStream(
+							new FileInputStream(
+									"WebContent/classifiers/ner-eng-ie.crf-4-conll.ser.gz")));
+			DefaultTextProcessingFacade tf = new DefaultTextProcessingFacade(is);
+			System.out.println(tf.listStems(input));
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-
 	}
+
+	/*
+	 * public static void main(String[] args) { try { String input =
+	 * "Carl Edward Sagan (November 9, 1934 – December 20, 1996) " +
+	 * "was an American astronomer, astrophysicist, cosmologist, author, science popularizer, "
+	 * + "and science communicator in the space and natural sciences.";
+	 * InputStream is = new GZIPInputStream(new BufferedInputStream( new
+	 * FileInputStream( "classifiers/ner-eng-ie.crf-4-conll.ser.gz")));
+	 * DefaultTextProcessingFacade tf = new DefaultTextProcessingFacade(is);
+	 * 
+	 * System.out.println(tf.getEntities(input)); } catch (Exception e) { throw
+	 * new RuntimeException(e); }
+	 * 
+	 * }
+	 */
 }
