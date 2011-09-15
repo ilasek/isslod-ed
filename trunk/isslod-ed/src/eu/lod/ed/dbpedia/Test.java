21d74d994fe10684aae06e88c57489548e02153e
ivo.lasek@gmail.com <ivo.lasek@gmail.com@a6d0b3e4-eef0-53aa-bf60-0259151e4dc4>
2011-09-15 07:38:40 +0000

    DbpEntity added.

--- trunk/isslod-ed/src/eu/lod/ed/dbpedia/Test.java
+++ trunk/isslod-ed/src/eu/lod/ed/dbpedia/Test.java
@@ -6,10 +6,10 @@ public class Test {
     
     public static void main(String[] args) {
         DbpEndpoint endpoint = new DbpEndpoint("http://live.dbpedia.org/sparql");
-        List<DbpEntity> entities = endpoint.getDbpEntitiesByLabel("Berlin");
+        List<DbpEntity> entities = endpoint.getDbpEntitiesByLabel("Paris");
         
         for (DbpEntity entity : entities) {
-            System.out.println(entity.getLabel() + " " + entity.getUri() + " " + entity.getRdfType());
+            System.out.println(entity.getUri() + " " + entity.getDescription());
         }
     }
     
